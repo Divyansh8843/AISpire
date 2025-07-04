efe37a3a327abf619c9fa6a9080e05399fa98696
Divyansh8843 <divyanshagrawal8843@gmail.com>
2025-07-04 15:59:16 +0530

    eslintignore added

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  // ...other config
   images: {
     domains: ["media.licdn.com"],
   },
 };
-
 export default nextConfig;
